40c75c7fcd265c1b0addfeb02db9d0c2f7d5ff66
Henrique Coli Schumann <henrique.schumann@tapps.com.br>
2018-11-30 14:02:18 -0200

    create aula_2 project

---
+++ Curso_Unity_Intermediario_1_aula_2/Assets/_Scripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Health : MonoBehaviour {
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public RectTransform healthBar;
+
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!!!!");
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Curso_Unity_Intermediario_1_aula_2/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Utility;
+
+public class PlayerController : NetworkBehaviour {
+    private readonly float rotationSpeed = 300f;
+    private readonly float translationSpeed = 6f;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+        Camera.main.GetComponent<SmoothFollow>().target = transform;
+    }
+
+    [Command]
+    void CmdFire()
+    {
+
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation
+            );
+
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, 2.0f);
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+        if (isLocalPlayer)
+        {
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * translationSpeed;
+
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, 0, z);
+
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                CmdFire();
+                CmdFire();
+                CmdFire();
+                CmdFire();
+                CmdFire();
+            }
+        }
+	}
+}
